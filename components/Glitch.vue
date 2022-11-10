ba40155387ef5d2d3ec734926f3f7d6438937441
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-10 11:57:41 +0700

    Develop Only

---
+++ components/Glitch.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Tets</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Glitch />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
